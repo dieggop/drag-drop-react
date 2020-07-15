a54d684583a0c88fd13a79a7d538d72cd08dafd7
Dieggo Siqueira <dieggo.siqueira@green.local>
2020-07-15 09:55:02 -0300

    Drag & Drop básico com react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <main className="boards">
+         <Board id="board-1" className="board">
+           <Card id="card-1" className="card" draggable="true">Card 1</Card>
+           <Card id="card-2" className="card" draggable="true">Card 2</Card>
+           <Card id="card-3" className="card" draggable="false">Card 3</Card>
+         </Board>
+         <Board id="board-2" className="board">
+
+         </Board>
+         <Board id="board-3" className="board">
+
+         </Board>
+       </main>
     </div>
   );
 }
